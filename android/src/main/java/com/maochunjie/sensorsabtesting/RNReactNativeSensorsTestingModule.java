0f31f6d3c3492f325071c43a9049a501aa220057
孑愁 <401828628@qq.com>
2025-04-16 21:30:36 +0800

    集成Android SDK

--- android/src/main/java/com/maochunjie/sensorsabtesting/RNReactNativeSensorsTestingModule.java
+++ android/src/main/java/com/maochunjie/sensorsabtesting/RNReactNativeSensorsTestingModule.java
@@ -1,10 +1,16 @@
 package com.maochunjie.sensorsabtesting;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
 import com.facebook.react.bridge.ReadableMap;
+import com.sensorsdata.abtest.SensorsABTest;
+import com.sensorsdata.abtest.OnABTestReceivedData;
+import com.sensorsdata.abtest.SensorsABTestConfigOptions;
 
 public class RNReactNativeSensorsTestingModule extends ReactContextBaseJavaModule {
     private final ReactApplicationContext reactContext;
@@ -19,7 +25,62 @@ public class RNReactNativeSensorsTestingModule extends ReactContextBaseJavaModul
         return "RNReactNativeSensorsTesting";
     }
 
+    // 初始化神策 A/B Testing SDK
     @ReactMethod
-    public void init(final ReadableMap data, final Promise p) {
+    public void init(ReadableMap params, final Promise promise) {
+        try {
+            String serverUrl = params.getString("serverUrl");
+            if (serverUrl != null && !serverUrl.isEmpty()) {
+                Log.d("SensorsTesting", "Initializing SensorsABTest with server URL: " + serverUrl);
+                // 创建配置选项
+                SensorsABTestConfigOptions abTestConfigOptions = new SensorsABTestConfigOptions(serverUrl);
+                // 初始化 A/B Testing SDK
+                SensorsABTest.startWithConfigOptions(reactContext, abTestConfigOptions);
+            }
+            Log.d("SensorsTesting", "SensorsABTest initialized successfully.");
+        } catch (Exception e) {
+            Log.e("SensorsTesting", "Error initializing SensorsABTest", e);
+        }
+        promise.resolve("succeed");
+    }
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public String fetchCacheABTest(final String experimentKey, final String defaultValue) {
+        try {
+            return SensorsABTest.shareInstance().fetchCacheABTest(experimentKey, defaultValue);
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+
+    @ReactMethod
+    public void asyncFetchABTest(final String experimentKey, final String defaultValue, final Promise promise) {
+        try {
+            SensorsABTest.shareInstance().asyncFetchABTest(experimentKey, defaultValue, new OnABTestReceivedData<String>() {
+                @Override
+                public void onResult(String result) {
+                    promise.resolve(result);
+                }
+            });
+        } catch (Exception e) {
+            Log.d("SensorsTesting", "SensorsABTest" + e);
+            promise.resolve(defaultValue);
+        }
+    }
+
+    @ReactMethod
+    public void fastFetchABTest(final String experimentKey, final String defaultValue, final Promise promise) {
+        try {
+            SensorsABTest.shareInstance().fastFetchABTest(experimentKey, defaultValue, new OnABTestReceivedData<String>() {
+                @Override
+                public void onResult(String result) {
+                    promise.resolve(result);
+                }
+            });
+        } catch (Exception e) {
+            // 捕获异常并通过 Promise 返回错误信息
+            Log.d("SensorsTesting", "SensorsABTest" + e);
+            promise.resolve(defaultValue);
+        }
     }
 }
